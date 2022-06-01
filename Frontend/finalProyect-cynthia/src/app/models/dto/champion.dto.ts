8313434deb0118eb0a8821236b372f31adaf2cf7
Cynthia Labrador <cynthialabrador47@gmail.com>
2022-06-01 15:46:30 +0200

    (web): redesign

--- Frontend/finalProyect-cynthia/src/app/models/dto/champion.dto.ts
+++ Frontend/finalProyect-cynthia/src/app/models/dto/champion.dto.ts
@@ -2,11 +2,9 @@ export class ChampionDto {
   name: string;
   cost: number;
   traitsUuid: number[];
-  file: string;
   constructor() {
       this.name = '';
       this.cost = 0;
       this.traitsUuid = [];
-      this.file = 'https://user-images.githubusercontent.com/24848110/33519396-7e56363c-d79d-11e7-969b-09782f5ccbab.png';
   }
 }
