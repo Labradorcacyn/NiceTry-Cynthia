aa22d0ab3c4e1e2ca42ed66948288c4cb8827c85
Cynthia Labrador Caballero <cynthialabrador47@gmail.com>
2022-06-07 18:20:41 +0200

    (web): paginate champs and filter

--- Frontend/finalProyect-cynthia/src/app/screens/champion-list/champion-list.component.ts
+++ Frontend/finalProyect-cynthia/src/app/screens/champion-list/champion-list.component.ts
@@ -9,14 +9,21 @@ import { ChampionService } from 'src/app/services/champion.service';
 })
 export class ChampionListComponent implements OnInit {
 
-  champions: ChampionResponse[] | undefined;
+  champions!: ChampionResponse[];
+  filtered!: ChampionResponse[];
+  nameTr!: string;
+  p!: number;
 
   constructor(private championService: ChampionService) { }
 
   ngOnInit(): void {
     this.championService.getChampions().subscribe(champions => {
       this.champions = champions;
+      this.filtered = champions;
     });
   }
 
+  getName(){
+    this.filtered = this.champions.filter((ch) => ch.name.toLowerCase().includes(this.nameTr.toLowerCase()));
+}
 }
