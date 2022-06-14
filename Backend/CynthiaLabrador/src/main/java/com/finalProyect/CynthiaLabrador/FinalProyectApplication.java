579bea5fba9753462fd0737d5e51f0c5b05d81b1
Cynthia Labrador Caballero <cynthialabrador47@gmail.com>
2022-06-14 12:51:01 +0200

    (develop-api): swagger

--- Backend/CynthiaLabrador/src/main/java/com/finalProyect/CynthiaLabrador/FinalProyectApplication.java
+++ Backend/CynthiaLabrador/src/main/java/com/finalProyect/CynthiaLabrador/FinalProyectApplication.java
@@ -4,9 +4,11 @@ import com.finalProyect.CynthiaLabrador.config.StorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
+@EnableWebMvc
 public class FinalProyectApplication {
 
 	public static void main(String[] args) {
